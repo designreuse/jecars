e5d309215700ea06eeb0b7fd20a3d49c5720c89a
1111software <1111software@gmail.com>
2010-03-23 16:40:57 +0100

    - Copyright header

--- jecars_main/src/org/jecars/output/CARS_InputStream.java
+++ jecars_main/src/org/jecars/output/CARS_InputStream.java
@@ -1,8 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2010 NLR - National Aerospace Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.jecars.output;
 
 import java.io.File;
@@ -19,9 +29,8 @@ import java.util.logging.Logger;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-/**
+/** CARS_InputStream
  *
- * @author weert
  */
 public class CARS_InputStream extends InputStream {
 
@@ -118,6 +127,7 @@ public class CARS_InputStream extends InputStream {
           LOG.log( Level.WARNING, ex.getMessage(), ex );
         }
         len = mStream.read( pBuffer, pOff, pLen );
+    System.out.println(" DELAY READ RES = " + len + " tot=" + mBytesReaden );
         if (len>0) {
           break;
         }
