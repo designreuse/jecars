9e28c971e71fad80474fe511550c5e7ca34fa0fb
weert <weert@Latn007w>
2010-04-09 15:59:34 +0200

    - MailManager

--- jecars_main/src/org/jecars/CARS_MailManager.java
+++ jecars_main/src/org/jecars/CARS_MailManager.java
@@ -16,8 +16,12 @@
 
 package org.jecars;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,19 +51,34 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
 
   private static final Object LOCK = new Object();
 
-  private transient volatile boolean        mMailToBeSend = false;
-  private transient Session                 mSession = null;
-  private transient long                    mCheckPathLastModified = 0L;
-  private transient final Map<String, Node> mCheckPaths = new ConcurrentHashMap<String, Node>();
+  private transient volatile boolean          mMailToBeSend = false;
+  private transient Session                   mSession = null;
+  private transient long                      mCheckPathLastModified = 0L;
+  private transient final Map<Node, String>   mCheckPaths = new ConcurrentHashMap<Node, String>();
 
   static private final long  CHECKEVERY                 = 22000L; // **** 22 seconds
+
+  /** getCheckPathNode
+   *
+   * @param pValue
+   * @return
+   */
+  private List<Node> getCheckPathNode( final String pValue ) {
+    List<Node> res = new ArrayList<Node>();
+    Iterator<Node> it = mCheckPaths.keySet().iterator();
+    while( it.hasNext() ) {
+      Node n = it.next();
+      if (mCheckPaths.get( n ).equals( pValue )) {
+        res.add( n );
+      }
+    }
+    return res;
+  }
 
   /** onEvent_Mails
    *
    */
   private class onEvent_Mails implements EventListener {
-
-
 
     /** onEvent
      *
@@ -74,63 +93,64 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
             lastEvent = pEI.nextEvent();
             final String path = lastEvent.getPath();
 //            System.out.println("MAIL: EVENT ADDED = " + path );
-            final Iterator<String> keys = mCheckPaths.keySet().iterator();
-            while( keys.hasNext() ) {
-              final String key = keys.next();
+            final Map<String, String> handled = new HashMap<String, String>();
+            final Collection<String> keys = mCheckPaths.values();
+            for( final String key : keys) {
 //            System.out.println(" EVENT KEY " + key );
-              if (path.startsWith( key )) {
+              if ((!handled.containsKey( key )) && (path.startsWith( key ))) {
+                handled.put( key, key );
 //            System.out.println("CREATE MAIL: EVENT ADDED = " + path );
                 synchronized(LOCK) {
-                final Node linkNode = mCheckPaths.get( key );
-                if (linkNode!=null) {
-                  final Node eventNode = mSession.getNode( path );
-                  boolean ok = true;
-                  if (linkNode.hasProperty( "jecars:EventType" )) {
-                    ok = lastEvent.getType()==linkNode.getProperty( "jecars:EventType" ).getLong();
-                  }
-                  if ((ok) && (linkNode.hasProperty( "jecars:NodeType" ))) {
-                    ok = eventNode.isNodeType( linkNode.getProperty( "jecars:NodeType" ).getString() );
-                  }
-                  if ((ok) && (linkNode.hasProperty( "jecars:User" ))) {
-                    final String princ = linkNode.getProperty( "jecars:User" ).getValue().getString();
-                    try {
-                      final Node princN = mSession.getNode( princ );
-                      if (princN.hasProperty( "jecars:Email" ) && princN.hasNode( "jecars:Prefs" )) {
-                        final Node prefs = princN.getNode( "jecars:Prefs" );
-                        if (!prefs.hasNode( "MailBox" )) {
-                          prefs.addNode( "MailBox", "jecars:MailBox" );
-                          prefs.save();
+                  List<Node> linkNodes = getCheckPathNode( key );
+                  for( final Node linkNode : linkNodes) {
+                    final Node eventNode = mSession.getNode( path );
+                    boolean ok = true;
+                    if (linkNode.hasProperty( "jecars:EventType" )) {
+                      ok = lastEvent.getType()==linkNode.getProperty( "jecars:EventType" ).getLong();
+                    }
+                    if ((ok) && (linkNode.hasProperty( "jecars:NodeType" ))) {
+                      ok = eventNode.isNodeType( linkNode.getProperty( "jecars:NodeType" ).getString() );
+                    }
+                    if ((ok) && (linkNode.hasProperty( "jecars:User" ))) {
+                      final String princ = linkNode.getProperty( "jecars:User" ).getValue().getString();
+                      try {
+                        final Node princN = mSession.getNode( princ );
+                        if (princN.hasProperty( "jecars:Email" ) && princN.hasNode( "jecars:Prefs" )) {
+                          final Node prefs = princN.getNode( "jecars:Prefs" );
+                          if (!prefs.hasNode( "MailBox" )) {
+                            prefs.addNode( "MailBox", "jecars:MailBox" );
+                            prefs.save();
+                          }
+                          final Node mailBox   = prefs.getNode( "MailBox" );
+                          final Node sendQueue = mailBox.getNode( "SendQueue" );
+                          final UUID uuid = UUID.randomUUID();
+                          final Node mail = sendQueue.addNode( "Mail_" + uuid, "jecars:Mail" );
+                          mail.setProperty( "jecars:InSendQueue", true );
+                          if (eventNode.hasProperty( "jecars:Title" )) {
+                            mail.setProperty( "jecars:Title", "JeCARS: " + eventNode.getProperty( "jecars:Title" ).getString() );
+                          } else {
+                            mail.setProperty( "jecars:Title", "JeCARS: " + eventNode.getPath() );
+                          }
+                          String body;
+                          if (eventNode.hasProperty( "jecars:Body" )) {
+                            body = eventNode.getProperty( "jecars:Body" ).getString();
+                          } else {
+                            body = lastEvent.toString();
+                          }
+                          body += "\n\n----------\nThis mail was automatically generated\n";
+                          mail.setProperty( "jecars:Body", body );
+                          CARS_Utils.setExpireDate( mail, (int)mailBox.getProperty( "jecars:NewMailExpire" ).getLong() );
+                          CARS_Utils.addMultiProperty( mail, "jecars:To",
+                                          princN.getProperty( "jecars:Email" ).getValue().getString(), false );
+                          sendQueue.save();
+                          mMailToBeSend = true;
                         }
-                        final Node mailBox   = prefs.getNode( "MailBox" );
-                        final Node sendQueue = mailBox.getNode( "SendQueue" );
-                        final UUID uuid = UUID.randomUUID();
-                        final Node mail = sendQueue.addNode( "Mail_" + uuid, "jecars:Mail" );
-                        mail.setProperty( "jecars:InSendQueue", true );
-                        if (eventNode.hasProperty( "jecars:Title" )) {
-                          mail.setProperty( "jecars:Title", "JeCARS: " + eventNode.getProperty( "jecars:Title" ).getString() );
-                        } else {
-                          mail.setProperty( "jecars:Title", "JeCARS: " + eventNode.getPath() );
-                        }
-                        String body;
-                        if (eventNode.hasProperty( "jecars:Body" )) {
-                          body = eventNode.getProperty( "jecars:Body" ).getString();
-                        } else {
-                          body = lastEvent.toString();
-                        }
-                        body += "\n\n----------\nThis mail was automatically generated\n";
-                        mail.setProperty( "jecars:Body", body );
-                        CARS_Utils.setExpireDate( mail, (int)mailBox.getProperty( "jecars:NewMailExpire" ).getLong() );
-                        CARS_Utils.addMultiProperty( mail, "jecars:To",
-                                        princN.getProperty( "jecars:Email" ).getValue().getString(), false );
-                        sendQueue.save();
-                        mMailToBeSend = true;
+                      } catch( Exception pe ) {
+                        pe.printStackTrace();
+                        // **** No action, because otherwise it would generate an event
                       }
-                    } catch( Exception pe ) {
-                      pe.printStackTrace();
-                      // **** No action, because otherwise it would generate an event
                     }
                   }
-                }
                 }
               }
             }
@@ -171,12 +191,12 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
 
 //    final Node tool = mSession.getNode( getTool().getPath() );
     final Node mailLinks = mSession.getNode( getTool().getPath() + "/MailLinks" );
-    if ((mCheckPathLastModified==0) || (mailLinks.hasProperty( "jcr:lastModified" ))) {
+    if ((mCheckPathLastModified==0) || (mailLinks.hasProperty( "jecars:Modified" ))) {
       final long mod;
       if (mCheckPathLastModified==0) {
         mod = System.currentTimeMillis();
       } else {
-        mod = mailLinks.getProperty( "jcr:lastModified" ).getLong();
+        mod = mailLinks.getProperty( "jecars:Modified" ).getLong();
       }
 //    System.out.println("CHECK MAIL : " + mod);
       if (mCheckPathLastModified!=mod) {
@@ -186,11 +206,12 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
         mCheckPaths.clear();
         while( links.hasNext() ) {
           final Node link = links.nextNode();
-          mCheckPaths.put( link.getProperty( "jecars:ObservationPath" ).getValue().getString(), link );
+          mCheckPaths.put( link, link.getProperty( "jecars:ObservationPath" ).getValue().getString() );
         }
       }
     }
 
+    // *************************************
     // **** Check for mails
     if (mMailToBeSend) {
       mMailToBeSend = false;
@@ -215,8 +236,8 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
               final String result = email.send();
               final Node mailbox = mailNode.getParent().getParent();
         //        System.out.println("result = " + result );
-              mailNode.setProperty( "jecars:SendResult", result );
-              mailNode.setProperty( "jecars:SendedAt", Calendar.getInstance() );
+              mailNode.setProperty( "jecars:SendResult",  result );
+              mailNode.setProperty( "jecars:SendedAt",    Calendar.getInstance() );
               mailNode.setProperty( "jecars:InSendQueue", false );
               CARS_Utils.setCurrentModificationDate( mailNode );
               CARS_Utils.setExpireDate( mailNode, (int)mailbox.getProperty( "jecars:SendedMailExpire" ).getLong() );
@@ -224,8 +245,8 @@ public class CARS_MailManager extends CARS_DefaultToolInterface {
 //        System.out.println("result " + email.send() );
               // **** Check if there is a Sended box
               if (mailbox.isNodeType( "jecars:MailBox" )) {
-                mailbox.setProperty( "jecars:LastMailSend", Calendar.getInstance() );
-                dms.setProperty(     "jecars:LastMailSend", Calendar.getInstance() );
+                mailbox.setProperty( "jecars:LastMailSend",   Calendar.getInstance() );
+                dms.setProperty(     "jecars:LastMailSend",   Calendar.getInstance() );
                 mailbox.setProperty( "jecars:TotalMailsSend", mailbox.getProperty( "jecars:TotalMailsSend" ).getLong()+1 );
                 dms.setProperty(     "jecars:TotalMailsSend", dms.getProperty( "jecars:TotalMailsSend" ).getLong()+1 );
                 CARS_Utils.setCurrentModificationDate( mailbox );
